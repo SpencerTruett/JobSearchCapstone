246f210c7c15617a767936c5ab001983b5cb2645
Spencer Truett <srtruett1993@gmail.com>
2020-05-02 21:16:35 -0500

    added company model

---
+++ Models/Company.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public int LocationId { get; set; }
+        public string AboutUs { get; set; }
+    }
+}
