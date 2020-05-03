935b9d9e848530d4ff38561313afbf680a6e7321
Spencer Truett <srtruett1993@gmail.com>
2020-05-02 21:25:40 -0500

    added user model

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public int ApplicantId { get; set; }
+        public bool IsEmployer { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int LocationId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
