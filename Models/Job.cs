089b14db6130cba2d6b8481d1df8a293fd07dfab
Spencer Truett <srtruett1993@gmail.com>
2020-05-02 21:29:04 -0500

    added job model

---
+++ Models/Job.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.Models
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Position { get; set; }
+        public string Description { get; set; }
+        public string Salary { get; set; }
+        public string YearsOfExperience { get; set; }
+        public int EmploymentTypeId { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
