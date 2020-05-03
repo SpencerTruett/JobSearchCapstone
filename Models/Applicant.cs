63fa33c124bda1b4d476a339e5870f9c92a41f9a
Spencer Truett <srtruett1993@gmail.com>
2020-05-02 21:22:36 -0500

    added applicant model

---
+++ Models/Applicant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.Models
+{
+    public class Applicant
+    {
+        public int Id { get; set; }
+        public string image { get; set; }
+        public string SocialLink { get; set; }
+        public string Education { get; set; }
+        public string Experience { get; set; }
+        public string SkillsAndCertifications { get; set; }
+        public string Resume { get; set; }
+    }
+}
