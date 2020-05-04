40740ad9b956f3a385633e15a7584bcc44319eb9
Spencer Truett <srtruett1993@gmail.com>
2020-05-04 10:16:45 -0500

    added EmployerDashboardViewModel

---
+++ Models/ViewModels/EmployerDashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.Models.ViewModels
+{
+    public class EmployerDashboard
+    {
+        public Company Company { get; set; }
+        public Job Job { get; set; }
+    }
+}
