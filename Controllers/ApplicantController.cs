3adc5c2953e5048f5530206fb8ba412f3c40d94a
Spencer Truett <srtruett1993@gmail.com>
2020-05-05 13:18:11 -0500

    added initial controllers

---
+++ Controllers/ApplicantController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobSearch.Controllers
+{
+    public class ApplicantController : Controller
+    {
+        // GET: Applicant
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Applicant/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Applicant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Applicant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Applicant/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Applicant/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Applicant/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Applicant/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
