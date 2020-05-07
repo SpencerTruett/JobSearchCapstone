0af52c32c2fb20f88425cc742f2fdde78510a29e
Spencer Truett <srtruett1993@gmail.com>
2020-05-07 15:03:26 -0500

    create employerCompanyViewModel

---
+++ Models/ViewModels/EmployerCompanyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.Models.ViewModels
+{
+    public class EmployerCompanyViewModel
+    {
+        public int Id { get; set; }
+        public Company Company { get; set; }
+        public int? LocationId { get; set; }
+        public List<SelectListItem> LocationOptions { get; set; }
+    }
+}
